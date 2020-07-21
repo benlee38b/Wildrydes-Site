7c737ba4ea8eff04257c24cce547d69771fdeef4
Ben Lee <benjamintlee600@gmail.com>
2020-07-21 11:11:23 +0100

    updated config file

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: ' eu-west-2_uLYoLaeel', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '3eom1qvc19olf84hpjorre6rf1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'eu-west-2', // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
