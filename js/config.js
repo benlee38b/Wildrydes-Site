836e1c7d111b0d48f6d23680323ee1482e4b24b9
Ben Lee <benjamintlee600@gmail.com>
2020-07-21 11:35:36 +0100

    made small change to config file

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
   cognito: {
-    userPoolId: ' eu-west-2_uLYoLaeel', // e.g. us-east-2_uXboG5pAb
+    userPoolId: 'eu-west-2_uLYoLaeel', // e.g. us-east-2_uXboG5pAb
     userPoolClientId: '3eom1qvc19olf84hpjorre6rf1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: 'eu-west-2', // e.g. us-east-2
   },
